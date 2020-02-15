f9a5af79617eae8051687b3645a853644f40ba82
sszukhent <commandershepard989@gmail.com>
2020-02-15 10:06:55 -0500

    post route commit

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,9 +1,39 @@
 const express = require('express');
 const router = express.Router();
+const { check, validationResult } = require('express-validator');
+const auth = require('../../middleware/auth');
+const Profile = require('../../models/Profile');
+const User = require('../../models/User');
+const Post = require('../../models/Post');
 
-// @route GET api/posts
-// @desc Test Route
-// @access Public
-router.get('/', (req, res) => res.send('Posts route'));
+// @route POST api/posts
+// @desc Create a post
+// @access Private
+router.post(
+  '/',
+  [
+    auth,
+    [
+      check('text', 'Text is required')
+        .not()
+        .isEmpty()
+    ]
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      res.status(400).json({ errors: errors.array() });
+    }
+
+    const user = await User.findById(req.user.id).select('-password');
+
+    const newPost = {
+      text: req.body.text,
+      name: user.name,
+      avatar: user.avatar,
+      user: req.user.id
+    };
+  }
+);
 
 module.exports = router;
